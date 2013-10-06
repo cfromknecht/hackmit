405b31a0a49a313618a857a033f7597054dd2bb9
Vaibhav Gupta <kacxdak@gmail.com>
2013-10-06 04:14:47 -0400

    Secure should work

--- secure.sh
+++ secure.sh
@@ -4,5 +4,6 @@ for i in $(ls question/1/*.in)
 do
 	base=$(echo $i | rev | cut -c 4- | rev)
 	# echo $base
-	diff <(python run_python_secure.py '''print 2'''<< $base".in") <($base".ans")
+	# diff <(python run_python_secure.py '''print 2'''<< $base".in") <($base".ans")
+	python run_python_secure.py '''print 2'''<< $base".in"
 done
