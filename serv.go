d0105a7f8b0be94272a7d91223667dabbdbd4541
Vaibhav Gupta <kacxdak@gmail.com>
2013-10-06 09:31:27 -0400

    adding changes to bash call

--- serv.go
+++ serv.go
@@ -192,7 +192,7 @@ func testCode(w http.ResponseWriter, r *http.Request) {
  //    	fmt.Println(err1)
 	// }
 	// fmt.Printf("The date is %s\n", out1)
-	out, err := exec.Command(app, qid, code).Output()
+	out, err := exec.Command("bash", "-c", "secure.sh " + qid + " '" + code + "'").Output()
     if err != nil {
     	fmt.Println("LOL", err)
     	return
