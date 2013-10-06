f5eaab2c36109ad0d7e234008e1e2f0357921716
Vaibhav Gupta <kacxdak@gmail.com>
2013-10-06 04:10:52 -0400

    Secure should work

--- run_python_secure.py
+++ run_python_secure.py
@@ -6,12 +6,16 @@ from StringIO import StringIO
 if __name__ == "main":
 	'''
 	Will run a given code in sandbox (stream stdio from cmd)
+	arg[1] = code given
+	to run
+	python run_python_secure.py <code>  < inputfile 
 	'''
 	if len(sys.argv) != 2:
 		print "Not enough args"
 		sys.exit()
 	args = sys.argv
 	code = args[1]
+	print "Will run: ", code
 	try:
 		sandbox = Sandbox(SandboxConfig('stdin', 'stdout'))
 		sandbox.io(sys.stdin, sys.stdout)
