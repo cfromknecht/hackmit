4f8465dcd825518388d8e307e8a21773719cd06d
Vaibhav Gupta <kacxdak@gmail.com>
2013-10-06 04:21:53 -0400

    Secure should work

--- secure.sh
+++ secure.sh
@@ -6,5 +6,5 @@ do
 	answer+=".ans"
 	# echo $base
 	# diff <(python run_python_secure.py '''print 2'''<< $base".in") <($base".ans")
-	python run_python_secure.py "x = raw_input()\nprint x" < $i
+	python run_python_secure.py '''x = raw_input()\nprint x''' < $i
 done
