3fdb84b4fbabc6389851e9637ae7bd881e3b58b0
Vaibhav Gupta <kacxdak@gmail.com>
2013-10-05 20:57:42 -0400

    Adding tests

--- run_python_secure.py
+++ run_python_secure.py
@@ -5,38 +5,39 @@ from StringIO import StringIO
 
 if __name__ == "main":
 	if len(sys.argv) != 3:
-		sys.exit("Not enough args")
+		print "Not enough args"
+		sys.exit()
 	args = sys.argv
-	problem = args[1]
-	# code = args[2]
-	code = '''while True:
-		pass
-	'''
-	diff = ""
+	problem = '1'
+	code = '''print 2'''
 	counter = 1
 	sandbox = Sandbox(SandboxConfig('stdin', 'stdout'))
-	for x in listdir('./question/' + problem):
-		if x[-4:] == ".ans":
+	for x in listdir('./question/' + problem ):
+		if x[-3:] == ".in":
+			print "Attempting: ", x
+			f = open(x)
+			fIn = StringIO(f.read())
+			f.close()
+			fOut = StringIO()
+			old_stdout = sys.stdout
+			old_stdin = sys.stdin
+			sys.stdout = fOut
+			sys.stdin = fIn
+			status = "NOT PROCESSED"
 			try:
-				fInput = StringIO(open(x).read())
-				result = StringIO()
-				sys.stdout = result
-				sys.stdin = fInput
 				sandbox.execute(code)
-				sys.stdout = sys.__stdout__
-				sys.stdin = sys.__stdin__
-				result_string = result.getvalue()
-				fInput_string = fInput.getvalue()
-				if result_string != fInput_string:
-					status = "FAIL"
-				else:
-					status = "PASS"
+				status = "PASS"
 			except Exception, e:
-				status = str(e)
-			finally:
-				diff = diff + "Test case " + str(counter) + ": " + status + "\n"
-				if status is not "PASS":
-					diff = diff + fInput_string + "\n"
-				diff = diff + "\n\n"
+				pass
+			sys.stdin = old_stdin
+			sys.stdout = old_stdout
+			result = fOut.getstring()
+			f = open(x[:-4] + ".ans")
+			excected = f.read()
+			f.close()
+			if result != excected:
+				status = "FAIL"
+			print "Test case " + str(counter) + ": " + status
+			if status != "PASS":
+				print fIn.getstring()
 			counter = counter + 1
-	print diff
