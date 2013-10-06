e582fdd49d0e66a90e3d777fd287ec973cfe343e
Vaibhav Gupta <kacxdak@gmail.com>
2013-10-06 04:38:51 -0400

    Secure should work

--- secure.sh
+++ secure.sh
@@ -4,5 +4,5 @@ for i in $(ls question/1/*.in)
 do
 	answer=$(echo $i | rev | cut -c 4- | rev)
 	answer=$answer".ans"
-	diff <(python run_python_secure.py "x = raw_input()\nprint x" < $i) <($answer)
+	diff <(python run_python_secure.py "x = raw_input()\nprint x" < $i) <(cat $answer)
 done
