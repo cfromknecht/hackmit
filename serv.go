d07eeeca93d51ab1f0b873f62a3c68a60efa90f6
Vaibhav Gupta <kacxdak@gmail.com>
2013-10-06 07:41:13 -0400

    adding changes to bash call

--- serv.go
+++ serv.go
@@ -185,7 +185,7 @@ func testCode(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	code := r.FormValue("submission")
 	_ = r.FormValue("cvid")
-	qid := 1
+	qid := '1'
 	app := "/home/suitup/hackmit/secure.sh"
 	out1, err1 := exec.Command("echo", string(qid), code).Output()
 	if err1 != nil {
